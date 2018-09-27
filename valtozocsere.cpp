fc9995da61208cb5ebcb60c02c0e37287020b19e
kzoltan99 <kzotya99@gmail.com>
2018-09-27 16:47:48 +0200

    Add files via upload

---
+++ valtozocsere.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int a=7;
+int b=1;
+
+cout<<"regi a = "<<a<<" b = "<<b<<"\n";
+
+a=a*b;
+b=a/b;
+a=a/b;
+
+cout<<"uj a = "<<a<<" b = "<<b<<"\n";
+}
---
+++ valtozocsere2.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+int a=6;
+int b=12;
+
+cout<<"regi a = "<<a<<" b = "<<b<<"\n";
+
+a=a-b;
+b=a+b;
+a=b-a;
+
+cout<<"uj a = "<<a<<" b = "<<b<<"\n";
+}
